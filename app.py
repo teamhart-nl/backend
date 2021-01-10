4e8c43a70a681bcc067767eadbde7261ab183e2a
Lourens Touwen <touwenlourens@gmail.com>
2021-01-10 12:07:48 +0100

    first skeleton code implementation w/ button and input to commandline (#1)

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('home.html')
+
+@app.route('/print/', methods=['POST'])
+def test():
+    if request.method == 'POST':
+        print(request.form['text'])
+        return('200')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

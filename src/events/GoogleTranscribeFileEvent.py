fa11fbbdf2a1ec761d7e7d02407068bb76bdf47f
rdvhoorn <38728066+rdvhoorn@users.noreply.github.com>
2021-01-25 17:22:02 +0100

    First implementation of google event
    also added a googleapi wrapper

--- src/events/GoogleTranscribeFileEvent.py
+++ src/events/GoogleTranscribeFileEvent.py
@@ -1,7 +1,12 @@
+import io
+
 from events.AbstractEvent import AbstractEvent
 from models.EventTypeEnum import EventType
 from models.request_data.AbstractRequest import AbstractRequest
 from models.request_data.TranscribeRequest import TranscribeRequest
+from modules.google_api.GoogleApiWrapper import GoogleApiWrapper
+
+from google.cloud import speech
 
 
 class GoogleTranscribeEvent(AbstractEvent):
@@ -12,11 +17,30 @@ class GoogleTranscribeEvent(AbstractEvent):
     PRIORITY: int = 200
 
     def __init__(self):
-        pass
+        self.client = speech.SpeechClient()
 
     def handle(self, request_data: AbstractRequest):
+        if not GoogleApiWrapper().authenticated:
+            raise AssertionError("GoogleTranscribeEvent.handle: make sure to authenticate with the Google API by "
+                                 "setting your credentials correctly.")
+
         if not isinstance(request_data, TranscribeRequest):
             raise ValueError("GoogleTranscribeEvent.handle: request_data is of type " + str(type(request_data)) + ".")
+
+        with io.open(request_data.path, "rb") as audio_file:
+            content = audio_file.read()
+
+        audio = speech.RecognitionAudio(content=content)
+        config = speech.RecognitionConfig(
+            request_data.audio_type,
+            language_code=request_data.spoken_language
+        )
+
+        response = self.client.recognize(config=config, audio=audio)
+
+        # Each result is for a consecutive portion of the audio. Iterate through
+        # them to get the transcripts for the entire audio file.
+        request_data.sentences = response.results
 
     @staticmethod
     def get_priority() -> int:
--- src/models/request_data/TranscribeRequest.py
+++ src/models/request_data/TranscribeRequest.py
@@ -1,6 +1,7 @@
 from models.EventTypeEnum import EventType
 from models.request_data.AbstractRequest import AbstractRequest
 
+from google.cloud import speech
 
 class TranscribeRequest(AbstractRequest):
     """
@@ -10,15 +11,24 @@ class TranscribeRequest(AbstractRequest):
     # Path to audio file
     path: str
 
+    # Type of audio file
+    audio_type: any
+
+    # Spoken language
+    spoken_language: str
+
     # Transcription
     sentences: [str]
 
-    def __init__(self, path):
+    def __init__(self, path: str, spoken_language: str):
         if not path or path == "":
             raise ValueError("TranscribeRequest.__init__: path variable is " + path)
 
         self.path = path
         self.sentences = []
+
+        self.audio_type = speech.RecognitionConfig.AudioEncoding.FLAC
+        self.spoken_language = spoken_language
 
     def get_event_type(self) -> EventType:
         return EventType.TRANSCRIBE_USING_GOOGLE_API

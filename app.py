b7010e9b1286c6eea5c5281e0c8d1fabd291fce3
LourensT <touwenlourens@gmail.com>
2021-02-01 13:19:58 +0100

    get phonemes endpoint

--- app.py
+++ app.py
@@ -3,20 +3,23 @@ from flask_cors import CORS
 
 from src.handlers.Dispatcher import Dispatcher
 from src.models.request_data.PhonemeTransformRequest import PhonemeTransformRequest
+from src.models.CMUPhonemes import CMUPhonemes
+
+import os
 
 app = Flask(__name__)
 CORS(app)
 
 dispatcher = Dispatcher()
 
-BASE_URL = "/api/v1/"
+BASE_URL = '/api/v1'
 
 @app.route('/vue-test')
 def vue_test():
     return {"greeting": "Hello from Flask!"}
 
 
-@app.route('/print/', methods=['POST'])
+@app.route(BASE_URL + '/print/', methods=['POST'])
 def test():
     if request.method == 'POST':
 
@@ -26,38 +29,15 @@ def test():
 
         return '200'
 
+@app.route(BASE_URL + '/phonemes')
+def phonemes():
+    available = []
+    for file in os.listdir(os.getcwd() + '\\resources\\phoneme_patterns'):
+        phoneme = file.replace(".json", "")
+        if phoneme in CMUPhonemes:
+            available.append(phoneme)
 
-#routes
-#GET pattern for a phoneme
-#param 
-
-#GET phonemes for sentence
-#described above
-
-#GET status of arduino
-#result: arduino status, motor status? idk if that accessible
-
-#POST phonemes for a given sentence
-#param: sl
-#param: tl
-#body: sentence
-#returns: phonemes (or patterns?)
-#effects: arduino fires full sentence
-
-#GET list of phonemes
-#phonemes with identifiable ID's 
-
-#GET stop arduino
-#effects: stops all motors from firing
-
-#GEt test arduino
-#effects: buzzes all motors
-
-#POST send single phoneme 
-#param: uniquely identifiable phoneme
-#returns: success or notFound
-#effects: arduino
-
+    return {'phonemes' : available}, 200
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=True)
